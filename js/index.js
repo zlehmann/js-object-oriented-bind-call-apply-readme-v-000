10df4f96ab06bf1ce867f898ac196d4f6de6b9d0
maxwellbenton <maxwellbenton@gmail.com>
2018-09-06 14:18:45 -0400

    added solution

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// use this to copy code snippets or us your browser's console
+class User {
+	constructor(name, favoriteBand) {
+		this.name = name;
+		this.favoriteBand = favoriteBand;
+	}
+	favoriteBandMatches(bands) {
+		console.log('in User scope: ', this.favoriteBand);
+		return bands.filter(
+			function(band) {
+				console.log('in the anonymous function scope: ', this.favoriteBand);
+				return band == this.favoriteBand;
+			}.bind(this)
+		)[0];
+	}
+}
+
+let billy = new User('billy', 'paul simon');
+billy.favoriteBandMatches(['paul simon', 'the kooks']);
